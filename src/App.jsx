a47c8f54134c4f65ecec4fd85ad976def8258c38
DDevNinja <patildurgesh4002@gmail.com>
2026-07-16 20:35:33 +0530

    Build error solve

--- src/App.jsx
+++ src/App.jsx
@@ -69,8 +69,8 @@
 import React, { useRef } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Agence from "./pages/agence";
-import Projects from "./pages/projects";
+import Agence from "./pages/Agence";
+import Projects from "./pages/Projects";
 import Navbar from "./Components/Navigation/Navbar";
 import FullScreenNav from "./Components/Navigation/FullScreenNav";
 const App = () => {
